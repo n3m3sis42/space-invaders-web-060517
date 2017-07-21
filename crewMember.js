1ec2e2e303aa784136ce6430fad37cceebacb2aa
github username <github email address>
2017-07-21 11:49:44 -0400

    progress

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,22 @@
+let shipWithCrew = {crewMembers: [], ships: []}
 
+
+function createCrewMember() {
+  let crewMemberCount = 0
+
+  return class CrewMember {
+    constructor(position) {
+      this.id = ++crewMemberCount
+      this.position = position
+
+    }
+    currentShip() {
+      return shipWithCrew.ships.filter( (ship) => {
+        return ship. === this.id
+      })
+    }
+  }
+
+
+
+}
---
+++ spaceship.js
@@ -0,0 +1,15 @@
+function createSpaceship() {
+  let spaceshipCount = 0
+
+  return class Spaceship {
+    constructor(position) {
+      this.id = ++spaceshipCount
+      this.crewMemberID = 
+
+    }
+
+  }
+
+
+
+}
